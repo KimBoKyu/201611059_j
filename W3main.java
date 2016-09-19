77eff8f52b266649fb4747ee873331de22448fb9
201611059 김보규 <sa5469@naver.com>
2016-09-20 00:34:25 +0900

    9월19일 과제 2-2, 3, 4

---
+++ W3main.java
@@ -0,0 +1,53 @@
+// homework 2_2 지하철
+class Subway{
+	int line;
+	void setLine(int l){
+		line = l;
+	}
+	void subwayRun(){
+		System.out.println(line + " line is run!!!");
+	}
+}
+
+Subway s = new Subway();
+s.setLine(4);
+s.subwayRun();
+
+// homework 2_3 커피원산지 
+class BrazilCoffee {
+	String origin;
+	int degreeOfRoast;
+	int grade;
+	BrazilCoffee(String o) {
+		origin=o;
+	}
+	public void roast() {
+		System.out.println("roasting..." + origin);
+	}
+	public void grind() {
+		System.out.println("grinding..." + origin);
+	}
+	public void brew() {
+		System.out.println("brewing..." + origin);
+	}
+}
+
+BrazilCoffee b = new BrazilCoffee("Brazil");
+b.roast();
+b.grind();
+b.brew();
+
+// homework 2_4 인사하기
+class Hello{
+	String name;
+	Hello(String name){
+		this.name=name;
+	}
+	void sayHello(){
+		System.out.println("Hello " + name);
+	}
+}
+
+Hello h = new Hello("world")
+h.sayHello();
+h.name;
